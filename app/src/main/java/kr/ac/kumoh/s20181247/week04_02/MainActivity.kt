0c0a767d3fd71ef967e027ccc436c3aac0eb2431
gkstj <gkstjswo122@gmail.com>
2022-09-28 14:46:25 +0900

    🎉프로젝트 시작

---
+++ app/src/main/java/kr/ac/kumoh/s20181247/week04_02/MainActivity.kt
@@ -0,0 +1,62 @@
+package kr.ac.kumoh.s20181247.week04_02
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.ac.kumoh.s20181247.week04_02.ui.theme.Week04_02Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    Week04_02Theme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            MyLinearLayout()
+        }
+    }
+}
+
+@Composable
+fun MyLinearLayout() {
+    Column() {
+        Text(text = "안녕하세요", modifier = Modifier
+            .background(Color.Cyan)
+            .padding(8.dp))
+        Text(text = "컴퓨터공학과")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp()
+}
